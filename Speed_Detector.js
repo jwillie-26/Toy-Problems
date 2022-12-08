5a02649286b4641414a84c08f07ed2226ba5f52c
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-12-08 14:38:13 +0300

    Update Speed_Detector.js

--- Speed_Detector.js
+++ Speed_Detector.js
@@ -8,26 +8,40 @@
     Speed < 70, deduct 1 point for every 5 km and print total number of demerit point
     suspend licence on more than 12 point
 */
-Let demeritPoints = 0;
-Let speedTracker = 0;
 
-
-function speedDetector(speed){
-    
-    //validate
-    if(speed < 70){
-        console.log("Speed is OK");
-    } else {
-        //speed-speed Limit , will give us excess the function will suspend license
-        Let newspeedPoints = (speed - 70)/5;
-        if(newspeedPoints <= 12 ) {
-            console.log("Demerit points " + newspeedPoints);
-        } else {
-            console.log("License Suspended")
-        }
+const readline = require('readline');
+const input = readline.createInterface(
+    {
+        input: process.stdin,
+        output: process.stdout
     }
-    return speed
-    
-}  
+);
+
+input.question("Speed of the car: ", function (speed){ 
+    let speed_of_car = parseInt(speed, 10);
+    if (isNaN(speed)){
+        process.stdout.write("Speed must be a numerical value\n");
+        process.stdout.write("use this a follows: On prompt input as numerical value e.g 50\n");
+        process.exit(1)
+    }else{
+       if (speed_of_car < 70){
+        process.stdout.write("Ok!\n");
+       } 
+        //speed-speed Limit , will give us excess the function will suspend license
+        else{
+        const demerit_points = ((speed_of_car - 70) / 5);
+        Math.ceil(demerit_points);
+        process.stdout.write(`Points: ${demerit_points}\n`);
+            if (demerit_points >= 12) {
+                process.stdout.write("License suspended.\n");                
+            } else {
+                process.exit(0);                
+            }
+       }
+    }
+    input.close();
+});
+        
+
 
 console.log(speedDetector(80));
