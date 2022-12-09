9f3cdc19727bf0f5227736f3775c7edf27b95a92
jwillie-26 <105637783+jwillie-26@users.noreply.github.com>
2022-12-09 10:17:51 +0300

    Update Speed_Detector.js

--- Speed_Detector.js
+++ Speed_Detector.js
@@ -9,39 +9,16 @@
     suspend licence on more than 12 point
 */
 
-const readline = require('readline');
-const input = readline.createInterface(
-    {
-        input: process.stdin,
-        output: process.stdout
+const detectSpeed = (speed) => {
+  if (speed < 70) {
+    console.log("OK");
+  } else {
+    let surpass = Math.floor((speed - 70) / 5);
+    if (surpass > 12) {
+      console.log("License suspended");
+    } else {
+      console.log(`Points: ${surpass}`);
     }
-);
-
-input.question("Speed of the car: ", function (speed){ 
-    let speed_of_car = parseInt(speed, 10);
-    if (isNaN(speed)){
-        process.stdout.write("Speed must be a numerical value\n");
-        process.stdout.write("use this a follows: On prompt input as numerical value e.g 50\n");
-        process.exit(1)
-    }else{
-       if (speed_of_car < 70){
-        process.stdout.write("Ok!\n");
-       } 
-        //speed-speed Limit , will give us excess the function will suspend license
-        else{
-        const demerit_points = ((speed_of_car - 70) / 5);
-        Math.ceil(demerit_points);
-        process.stdout.write(`Points: ${demerit_points}\n`);
-            if (demerit_points >= 12) {
-                process.stdout.write("License suspended.\n");                
-            } else {
-                process.exit(0);                
-            }
-       }
-    }
-    input.close();
-});
-        
-
-
-console.log(speedDetector(80));
+  }
+};
+// console.log(detectSpeed(100));
